b990e913006e682ae2956f091199b637ec308ec5
Lou Kaileh <lkaileh@gmail.com>
2017-03-16 21:27:36 -0700

    Used sequelize to setup our database schema

---
+++ server/config/db.js
@@ -0,0 +1,67 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('stackets', 'root', '', {dialect: 'postgres'}); //TBD to change the args (username , password) to ENV variables.
+
+// var User = db.define('User', {
+//   username: Sequelize.STRING
+// });
+var Snippet = db.define('Snippet', {
+  //id: Sequelize.INTEGER,
+  title: Sequelize.STRING,
+  snippet: Sequelize.TEXT,
+  shortDescription: Sequelize.STRING,
+  explanation: Sequelize.TEXT
+});
+
+var CodeSample = db.define('CodeSample', {
+  //id: Sequelize.INTEGER,
+  codeSample: Sequelize.TEXT
+});
+
+var Topic = db.define('Topic', {
+  //id: Sequelize.INTEGER,
+  name: Sequelize.TEXT
+});
+
+var Language = db.define('Language', {
+  //id: Sequelize.INTEGER,
+  name: Sequelize.TEXT,
+  version: Sequelize.TEXT
+});
+
+var Tags = db.define('Tags', {
+  //id: Sequelize.INTEGER,
+  tag: Sequelize.TEXT
+});
+
+var SnippetTags = db.define('SnippetTags', {
+  //id: Sequelize.INTEGER,
+});
+
+
+
+
+
+Snippet.sync()
+.then( () => CodeSample.sync() )
+.then( () => Topic.sync() )
+.then( () => Language.sync() )
+.then( () => Tags.sync() )
+.then( () => Snippet.belongsTo(Topic) )
+.then( () => Topic.hasMany(Snippet) )
+.then( () => Snippet.hasMany(CodeSample) )
+.then( () => CodeSample.belongsTo(Snippet) ) 
+.then( () => Snippet.belongsTo(Language) )
+.then( () => Language.hasMany(Snippet) )
+.then( () => Tags.hasMany(Snippet))
+.then( () => Snippet.hasMany(Tags))
+
+module.exports = {
+  Snippet: Snippet,
+  CodeSample: CodeSample,
+  Topic: Topic,
+  Language: Language,
+  Tags: Tags,
+  SnippetTags: SnippetTags
+}
+// Snippet_id: Sequelize.INTEGER,
+//   tag_id: Sequelize.INTEGER
