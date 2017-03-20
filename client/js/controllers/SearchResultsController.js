c0a3d5d5683fe0fa218857014123352e324dfc5f
Lou Kaileh <lkaileh@gmail.com>
2017-03-20 14:55:29 -0700

    Modified file and variable names to match with services and routes.  Also added controller and partials for Popular Snippets

--- client/js/controllers/SearchResultsController.js
+++ client/js/controllers/SearchResultsController.js
@@ -1,6 +1,6 @@
 angular.module('stackets.searchResults', [])
   .controller('SearchResultsController', function ($scope, Snippets) {
-    $scope.searchResultsTitle = "Search Results";
+    $scope.searchResultsTitle = 'Search Results';
     $scope.data = {};
     Snippets.getAllSnippets().then(function (snippets) {
       $scope.data.snippets = snippets;
