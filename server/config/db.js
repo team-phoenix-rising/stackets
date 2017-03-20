a3a11ee635252a5f0659b893d9f301742ad34b28
joannawheeler <joannachristinewheeler@gmail.com>
2017-03-20 14:00:20 -0700

    added api/snippets POST route

--- server/config/db.js
+++ server/config/db.js
@@ -47,60 +47,57 @@ Snippet.sync({force: true})
 .then(() => Tags.belongsToMany(Snippet, {through: SnippetTags, foreignkey: 'TagsId'}))
 .then(() => Snippet.belongsToMany(Tags, {through: SnippetTags, foreignkey: 'SnippetId'}))
 .then(() => SnippetTags.sync({force: true}))
-.then(() => {
-  // Insert default topics
-  Topic.create({ name: 'Javascript' });
-  Topic.create({ name: 'React' });
-  Topic.create({ name: 'Angular' });
-  Topic.create({ name: 'Database' });
-  Topic.create({ name: 'Server' });
-
+   // Insert default topics
+.then(() => { Topic.create({ name: 'Javascript' }) })
+.then(() => { Topic.create({ name: 'React' }) })
+.then(() => { Topic.create({ name: 'Angular' }) })
+.then(() => { Topic.create({ name: 'Database' }) })
+.then(() => { Topic.create({ name: 'Server' }) })
   // Insert default tags
-  Tags.create({ tag: 'ES5' });
-  Tags.create({ tag: 'ES6' });
-  Tags.create({ tag: 'ORM' });
-  Tags.create({ tag: 'SQL' });
-  Tags.create({ tag: 'Mongoose' });
-  Tags.create({ tag: 'Sequelize' });
-  Tags.create({ tag: 'D3' });
-  Tags.create({ tag: 'Backbone' });
-  Tags.create({ tag: 'Unicorns' });
-
+.then(() => { Tags.create({ tag: 'ES5' }) })
+.then(() => { Tags.create({ tag: 'ES6' }) })
+.then(() => { Tags.create({ tag: 'ORM' }) })
+.then(() => { Tags.create({ tag: 'SQL' }) })
+.then(() => { Tags.create({ tag: 'Mongoose' }) })
+.then(() => { Tags.create({ tag: 'Sequelize' }) })
+.then(() => { Tags.create({ tag: 'D3' }) })
+.then(() => { Tags.create({ tag: 'Backbone' }) })
+.then(() => { Tags.create({ tag: 'Unicorns' }) })
   // Insert dummy snippets
+.then(() => {
   Snippet.create({
     title: "Dummy title 1",
     snippet: "Dummy snippet 1",
     "shortDescription": "Dummy shortDescription 1",
     explanation: "Dummy explanation 1",
     "TopicId": 1
-  });
+  }) })
+.then(() => {
   Snippet.create({
     title: "Dummy title 2",
     snippet: "Dummy snippet 2",
     "shortDescription": "Dummy shortDescription 2",
     explanation: "Dummy explanation 2",
     "TopicId": 2
-  });
+  }) })
+.then(() => {
   Snippet.create({
     title: "Dummy title 3",
     snippet: "Dummy snippet 3",
     "shortDescription": "Dummy shortDescription 3",
     explanation: "Dummy explanation 32",
     "TopicId": 4
-  });
-
+  }) })
   // Insert dummy Snippet to Tag relations for join table
-  SnippetTags.create({ SnippetId: 1, TagId: 2});
-  SnippetTags.create({ SnippetId: 1, TagId: 5});
-  SnippetTags.create({ SnippetId: 2, TagId: 3});
-  SnippetTags.create({ SnippetId: 2, TagId: 4});
-
-  SnippetTags.create({ SnippetId: 2, TagId: 5});
-  SnippetTags.create({ SnippetId: 2, TagId: 8});
-  SnippetTags.create({ SnippetId: 3, TagId: 1});
-  SnippetTags.create({ SnippetId: 3, TagId: 9});
-  SnippetTags.create({ SnippetId: 3, TagId: 3});
-});
+.then(() => { SnippetTags.create({ SnippetId: 1, TagId: 2}) })
+.then(() => { SnippetTags.create({ SnippetId: 1, TagId: 5}) })
+.then(() => { SnippetTags.create({ SnippetId: 2, TagId: 3}) })
+.then(() => { SnippetTags.create({ SnippetId: 2, TagId: 4}) })
+.then(() => { SnippetTags.create({ SnippetId: 2, TagId: 5}) })
+.then(() => { SnippetTags.create({ SnippetId: 2, TagId: 8}) })
+.then(() => { SnippetTags.create({ SnippetId: 3, TagId: 1}) })
+.then(() => { SnippetTags.create({ SnippetId: 3, TagId: 9}) })
+.then(() => { SnippetTags.create({ SnippetId: 3, TagId: 3}) });
 
 module.exports = {
   Snippet: Snippet,
--- server/controllers/snippets-controller.js
+++ server/controllers/snippets-controller.js
@@ -6,64 +6,25 @@ module.exports = {
       .then(function(data) {
         console.log('inside findAll', data);
         res.status(200).json(data);
-        // {
-        //   id,
-        //   title,
-        //   snippet,
-        //   short description,
-        //   explanation,
-        //   topic_id: # --> topic table --> 'Javascript'
-        //   language_id: # --> language table --> 'ES6'
-        // }
       });
   },
   post: function(req, res) {
-    // req.body.alltags
-    // > current snippet id (findById(req.body.id))
-    // > in the join table Snippet_Tags,
-    // >
-
-    /*req.body -->
-      {
-        title: req.body.title
-        snippet: req.body.snippet,
-        shortDescription: req.body.shortDescription,
-        explanation: req.body.explanation,
-        TopicId: #
-        Tags: [#,#,#,#]
-      }
-
-      var tags = req.body.tags // depends what client calls tags array
-      */
-
-    // join table with the tag
-
-    // topic 'Javascript'
-    // --> look at topic table and find name 'Javascript'
-    // --> get the id of that entry
-    // // --> put into param
-    // db.Topic.findOne({name:req.body.topic})
-    //   .then(function(data) {
-    //     console.log(data.id);
-    //   });
-
     var params = {
       title: req.body.title,
       snippet: req.body.snippet,
       shortDescription: req.body.shortDescription,
-      explanation: req.body.explanation
-      // topicId
-      // tags - join table
+      explanation: req.body.explanation,
+      TopicId: req.body.TopicId,
     };
 
     db.Snippet.create(params)
-      .then(function (err, data) {
-        // db.SnippetTags.findById(data.id)
-        // then(function(err,data))
-          // forEach tags we get from client side,
-          // data.tags = req.body.tags
-
-        res.sendStatus(201);
+      .then(function (data) {
+        console.log(data.id)
+        var parsedTags = JSON.parse(req.body.Tags);
+        parsedTags.forEach(function(item) {
+          db.SnippetTags.create({ SnippetId: data.id, TagId: item })
+        })
+        res.status(201).json(data);
       });
   }
 };
