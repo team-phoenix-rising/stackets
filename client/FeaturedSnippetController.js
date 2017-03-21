4e24f4f0ba8545146207ee4af09bb71ec405d4e6
Lou Kaileh <lkaileh@gmail.com>
2017-03-21 14:15:33 -0700

    updated featured snippets partial and controller

--- client/FeaturedSnippetController.js
+++
@@ -1,4 +0,0 @@
-angular.module('stackets.featuredSnippet', [])
-  .controller('FeaturedSnippetController', function ($scope, Snippets) {
-    $scope.featuredSnippetTitle = "Featured Snippet";
-  });
