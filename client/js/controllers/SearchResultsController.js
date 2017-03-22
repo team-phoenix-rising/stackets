092109f8dde5ff3e6a5660b653c7ac09e619ffea
Lou Kaileh <lkaileh@gmail.com>
2017-03-22 15:25:53 -0700

    removed searchbar controller and partials from app.  Unified search results and implemented search filter

--- client/js/controllers/SearchResultsController.js
+++ client/js/controllers/SearchResultsController.js
@@ -2,6 +2,9 @@ angular.module('stackets.searchResults', [])
   .controller('SearchResultsController', function ($scope, Snippets) {
     $scope.searchResultsTitle = 'Search Results';
     $scope.data = {};
+    $scope.search = {
+      search: ''
+    }
     Snippets.getAllSnippets().then(function (snippets) {
       $scope.data.snippets = snippets;
     });
