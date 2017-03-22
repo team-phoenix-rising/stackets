7c4bd74695f17454a6718dc622bcd9365066b8c6
Danya D. Smith <danyadsmith@gmail.com>
2017-03-22 17:03:41 -0400

    Implements Tag Handling/Submission in the Add Snippet Form
    Updates the add-snippet.html partial to include checkboxes for
    tags.
    Updates the AddSnippetController to Populate Tags.
    Updates snippet data in danya.json
    Comments some code in FeaturedSnippetController to workaround
    console error.

--- client/js/controllers/FeaturedSnippetController.js
+++ client/js/controllers/FeaturedSnippetController.js
@@ -2,12 +2,12 @@ angular.module('stackets.featuredSnippet', [])
   .controller('FeaturedSnippetController', function ($scope, Snippets) {
     $scope.featuredSnippetTitle = "Featured Snippet";
     $scope.snippet = {};
-    Snippets.getfeaturedSnippet().then(function (snippets) {
-      $scope.snippet.title = snippets.title;
-      $scope.snippet.TopicId = snippets.TopicId;
-      $scope.snippet.shortDescription = snippets.shortDescription;
-      $scope.snippet.snippet = snippets.snippet;
-      $scope.snippet.description = snippets.description;
-      
-    });
+    // Snippets.getfeaturedSnippet().then(function (snippets) {
+    //   $scope.snippet.title = snippets.title;
+    //   $scope.snippet.TopicId = snippets.TopicId;
+    //   $scope.snippet.shortDescription = snippets.shortDescription;
+    //   $scope.snippet.snippet = snippets.snippet;
+    //   $scope.snippet.description = snippets.description;
+
+    // });
   });
