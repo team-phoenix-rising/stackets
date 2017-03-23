e1a4c0abd8d98aac9d185f90c24a4c328e6996dc
Danya D. Smith <danyadsmith@gmail.com>
2017-03-23 17:28:52 -0400

    Updates Snippets Factory with getSnippetById
    Adds function getSnippetById to the services.js file.
    
    Adds a call to this function in the ViewSnippetController.
    
    Now, when you click on a snippet title link in the Recent Snippets
    view or from the Search Results, you can see the snippet title in
    the View Snippet partial. The rest of the view snippet page UI is
    still pending.

--- client/js/controllers/ViewSnippetController.js
+++ client/js/controllers/ViewSnippetController.js
@@ -1,4 +1,9 @@
 angular.module('stackets.view', [])
   .controller('ViewSnippetController', function ($scope, Snippets, $stateParams) {
     console.log('Viewing Snippet No. ', $stateParams.id);
+    $scope.snippet = {};
+    Snippets.getSnippetById($stateParams.id).then(function (snippet) {
+      $scope.snippet = snippet;
+      //console.log('Metadata retrieved from Snippets service: ', JSON.stringify(topics));
+    });
   });
