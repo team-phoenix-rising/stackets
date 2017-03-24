547ad8bb71b1e11261d102b02071c282b0b170ad
Lou Kaileh <lkaileh@gmail.com>
2017-03-23 22:54:29 -0700

    Formatted Featured Snippet and SnippetView.  Added Sample stylesheet.  Added team section to About us.

--- client/js/controllers/FeaturedSnippetController.js
+++ client/js/controllers/FeaturedSnippetController.js
@@ -2,6 +2,9 @@ angular.module('stackets.featuredSnippet', [])
   .controller('FeaturedSnippetController', function ($scope, Snippets) {
     $scope.featuredSnippetTitle = "Featured Snippet";
     $scope.snippet = {};
+    Snippets.getSnippetById(4).then(function (snippet) {
+      $scope.snippet = snippet;
+      $scope.code = JSON.parse(snippet.snippet);
     // Snippets.getfeaturedSnippet().then(function (snippets) {
     //   $scope.snippet.title = snippets.title;
     //   $scope.snippet.TopicId = snippets.TopicId;
@@ -9,5 +12,5 @@ angular.module('stackets.featuredSnippet', [])
     //   $scope.snippet.snippet = snippets.snippet;
     //   $scope.snippet.description = snippets.description;
 
-    // });
+     });
   });
