ec9acc6102818e33eb0f7e6253ac2b612547927f
Bryce Dooley <brycehdooley@gmail.com>
2017-04-02 23:51:40 -0400

    After rebase

--- client/js/controllers/HomeController.js
+++ client/js/controllers/HomeController.js
@@ -1,7 +1,7 @@
 angular.module('stackets.home', [])
 
   .controller('HomeController', function ($scope, $http, $window, $location, Snippets) {
-    $scope.loggedIn = Snippets.getLogStatus();
+    // $scope.loggedIn = Snippets.getLogStatus();
     Snippets.authenticate().then(function(response) {
       console.log('autehnticating', $scope.loggedIn)
       if( $window.localStorage.stacketsToken ) {
