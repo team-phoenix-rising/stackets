00028ba6524359874da9e448c15e46d08ae4fa73
Emilie <emiliehester@gmail.com>
2017-04-03 17:30:13 -0600

    Use path to serve static directory

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var db = require('./config/db.js');
 var jwt = require('jsonwebtoken');
 var express = require('express');
 var session = require('express-session');
+var path = require('path');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var passport = require('passport');
@@ -14,18 +15,18 @@ passport.use(new Strategy({
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: 'http://localhost:3000/login/facebook/return',
     profileFields: ['id', 'displayName', 'photos', 'email']
-  }, function(accessToken, refreshToken, profile, done) {        
+  }, function(accessToken, refreshToken, profile, done) {
     db.User.findOrCreate({
-      where: {        
-        name: profile.displayName,        
+      where: {
+        name: profile.displayName,
         image: profile.photos[0].value,
         provider: profile.provider,
-        facebook_id: profile.id  
+        facebook_id: profile.id
       }
     })
     .then(function(user, err) {
       return done(err, user);
-    })    
+    })
 }));
 
 passport.use(new GitHubStrategy({
@@ -37,8 +38,8 @@ passport.use(new GitHubStrategy({
   function(accessToken, refreshToken, profile, done) {
     console.log('github profile..', profile)
     db.User.findOrCreate({
-      where: {        
-        name: profile.displayName,      
+      where: {
+        name: profile.displayName,
         image: profile._json.avatar_url,
         provider: profile.provider,
         github_id: profile.id
@@ -46,7 +47,7 @@ passport.use(new GitHubStrategy({
     })
     .then(function(user, err) {
       return done(err, user);
-    }); 
+    });
   }
 ));
 
@@ -69,7 +70,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, '../client')));
 
 require('./config/routes.js')(app, express);
 var port = process.env.PORT || 3000;
